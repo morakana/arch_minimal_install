b5ad82bdb5a567aa877d875723a20fca32b99ac9
morakana <sakamomo_yj@yahoo.co.jp>
2021-09-16 17:09:37 +0900

    Update phase03.sh

--- phase03.sh
+++ phase03.sh
@@ -28,7 +28,6 @@ fallocate -l $swapfilesize /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
-swapon --show
 echo ""
 echo "pkgfile --update"
 pkgfile --update
